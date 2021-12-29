07f6ca5161a78bdddc6cc9b77fc4e60e13ee376a
SergeyPavlovskiy <92663883+SergeyPavlovskiy@users.noreply.github.com>
2021-12-29 14:43:22 +0500

    2021

---
+++ my_code/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int[] array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] % 2 == 0)
+  {
+  	Console.Write($"{array[i]} ");
+  }
+}
